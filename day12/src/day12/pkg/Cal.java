9bd194bad3ad3ad68003c678b54b5b9c65d0a673
user <user@DESKTOP-T4A95VB>
2021-04-14 14:26:37 +0900

    Object-oriented Test 01

---
+++ day12/src/day12/pkg/Cal.java
@@ -0,0 +1,26 @@
+package day12.pkg;
+
+public class Cal {
+
+	int num1;
+	int num2;
+	
+	// 기본생성자
+	Cal(){
+		
+	}
+	
+	void sum1(int num1 , int num2) {
+		int sum = num1+num2;
+		System.out.println(sum);
+	}
+	
+	int sum2(int num1 , int num2) {
+		int sum = num1 + num2;
+		return sum;
+		
+	}
+	
+	
+	
+}
---
+++ day12/src/day12/pkg/CalMain.java
@@ -0,0 +1,20 @@
+package day12.pkg;
+
+public class CalMain {
+
+	public static void main(String[] args) {
+		
+				
+		Cal c1 = new Cal();
+		c1.sum1(10, 20);
+		
+		Cal c2 = new Cal();
+	    int number = c2.sum2(10, 20);  		
+		System.out.println(number);
+		
+		
+		
+		
+	}
+
+}
