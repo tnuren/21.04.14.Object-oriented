ccec6abdf844e2dadac82d05109d3933739fbe83
user <user@DESKTOP-T4A95VB>
2021-04-14 09:22:33 +0900

    그냥 이름이랑 학번만 바꿈

--- day12/src/day12/pkg/StudentMain.java
+++ day12/src/day12/pkg/StudentMain.java
@@ -7,13 +7,13 @@ public class StudentMain {
 		
 		Student s1 = new Student();
 		
-		s1.name = "손흥민";
-		s1.stuNum = 21;
+		s1.name = "학생1";
+		s1.stuNum = 111111;
 		System.out.println(s1.name);
 		System.out.println(s1.stuNum);
 		
 		// 매개변수가 있는 생성자를 이용한 객체선언
-		Student s2 = new Student("박지성" , 20);
+		Student s2 = new Student("학생2" , 222222);
 		System.out.println(s2.name);
 		System.out.println(s2.stuNum);
 
