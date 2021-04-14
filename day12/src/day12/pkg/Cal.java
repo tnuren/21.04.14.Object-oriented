158a5fedc752a7734b50d06d7ee95861c6a850ed
user <user@DESKTOP-T4A95VB>
2021-04-14 14:49:05 +0900

    메소드 설명추가

--- day12/src/day12/pkg/Cal.java
+++ day12/src/day12/pkg/Cal.java
@@ -9,7 +9,8 @@ public class Cal {
 	Cal(){
 		
 	}
-	
+	// 메소드를 정의할 때
+	// 리턴타입 , 메소드이름 , 매개변수유무 , 실행내용(리턴유무)체크
 	void sum1(int num1 , int num2) {
 		int sum = num1+num2;
 		System.out.println(sum);
--- day12/src/day12/pkg/CalMain.java
+++ day12/src/day12/pkg/CalMain.java
@@ -4,10 +4,11 @@ public class CalMain {
 
 	public static void main(String[] args) {
 		
-				
+		// 리턴이 없는 메소드		
 		Cal c1 = new Cal();
 		c1.sum1(10, 20);
 		
+		// 리턴이 있는 메소드
 		Cal c2 = new Cal();
 	    int number = c2.sum2(10, 20);  		
 		System.out.println(number);
